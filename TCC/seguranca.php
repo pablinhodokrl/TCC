fab0bd4db9309292f251a6941607bca869360382
pablo <85958068+pablinhodokrl@users.noreply.github.com>
2022-11-25 10:52:34 -0300

    Delete TCC directory

--- TCC/seguranca.php
+++
@@ -1,9 +0,0 @@
-<?php
-//ob start tira os espaços em branco
-ob_start();
-session_start();
-//VERIFICACAO PARA VER SE O USUARIO ESTÁ LOGADO
-if(($_SESSION['email']== "") || ($_SESSION['senha']== "")){
-    header("Location:index.php");
-}
-?>
